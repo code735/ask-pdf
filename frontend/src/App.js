e0d95ea173223a3673cf2c800675367030e27c42
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2025-01-08 08:41:55 +0530

    repo init

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import MasterContainer from './Components/MasterContainer/MasterContainer';
+import logo from './logo.svg';
+import './Styles/App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <MasterContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MasterContainer from './Components/MasterContainer/MasterContainer';
+import './Styles/App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <MasterContainer/>
+    </div>
+  );
+}
+
+export default App;
