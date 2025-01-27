0353b9ad7218f2ec69c0039e30120579a9543d3f
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2025-01-27 20:38:24 +0530

    pdf edits

--- frontend/dist/App.js
+++ frontend/dist/App.js
@@ -1,17 +1,42 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = __importDefault(require("react"));
+var react_1 = __importStar(require("react"));
 require("./Styles/App.scss");
+var material_1 = require("@mui/material");
+var icons_material_1 = require("@mui/icons-material");
 function App() {
-<<<<<<< HEAD
+    // states
+    var _a = (0, react_1.useState)(false), open = _a[0], setOpen = _a[1];
+    // functions
+    var handleClose = function () {
+    };
     return (react_1.default.createElement("div", { className: "App" },
-        react_1.default.createElement("h1", null, "Start Building")));
-=======
-    return (react_1.default.createElement("div", { className: "App" }, "Start Building."));
->>>>>>> bd1d47fb979c6ffedd3eeb2292231a6db1507edf
+        react_1.default.createElement(material_1.Button, { onClick: function () { } },
+            react_1.default.createElement(icons_material_1.InsertLink, null)),
+        react_1.default.createElement(material_1.Modal, { open: open, onClose: handleClose, "aria-labelledby": "modal-modal-title", "aria-describedby": "modal-modal-description" })));
 }
 exports.default = App;
 //# sourceMappingURL=App.js.map
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import MasterContainer from './Components/MasterContainer/MasterContainer';
+import React, { useState } from 'react';
 import './Styles/App.scss'
+import { Button, Modal } from '@mui/material';
+import { FamilyRestroomRounded, InsertLink } from '@mui/icons-material';
 
 function App() {
+
+  // states
+  const [open, setOpen] = useState(false)
+
+  // functions
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
   return (
     <div className="App">
-      {/* <MasterContainer/> */}
-      <h1>Start Building</h1>
+      <Button onClick={handleOpen}>
+        Uploaaaaaaaad <InsertLink />
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div>
+          <h3>PDF upload</h3>
+        </div>
+      </Modal>
     </div>
   );
 }
