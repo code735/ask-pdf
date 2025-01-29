75e2794dbb8c87ae9448e81410434b0f95e1b5d5
Sohail <sohailliyakatshaikh@gmail.com>
2025-01-29 20:37:16 +0530

    added UI (Unfinished)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,16 @@
 import React, { useState } from 'react';
 import './Styles/App.scss'
-import { Button, Modal } from '@mui/material';
-import { FamilyRestroomRounded, InsertLink } from '@mui/icons-material';
+import MasterContainer from './Components/MasterContainer/MasterContainer';
 
 function App() {
 
   // states
-  const [open, setOpen] = useState(false)
 
   // functions
-  const handleClose = () => {
-    setOpen(false)
-  }
-
-  const handleOpen = () => {
-    setOpen(true)
-  }
-
+ 
   return (
     <div className="App">
-      <Button onClick={handleOpen}>
-        Uploaaaaaaaad <InsertLink />
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <div>
-          <h3>PDF upload</h3>
-        </div>
-      </Modal>
+      <MasterContainer />
     </div>
   );
 }
--- frontend/src/Components/MasterContainer/MasterContainer.tsx
+++ frontend/src/Components/MasterContainer/MasterContainer.tsx
@@ -1,17 +1,48 @@
-import React from 'react'
-import Leftsidebar from '../Leftsidebar/Leftsidebar'
-import Pageeditor from '../Pageeditor/Pageeditor'
-import Rightsidebar from '../RightSidebar/Rightsidebar'
-import Navbar from '../Navbar/Navbar'
-
+import * as React from 'react'
+import { useState } from 'react';
+import { Button, Modal } from '@mui/material';
+import './MasterContainer.scss'
+import { FamilyRestroomRounded, InsertLink } from '@mui/icons-material';
+import LeftSidebar from '../Leftsidebar/LeftSidebar';
 export default function MasterContainer() {
+
+  const [open, setOpen] = useState(false)
+  const [inputValue, setInputValue] = useState("");
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setInputValue(event.target.files[0].name);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleOpen = () => {
+    document.getElementById("fileInput")?.click()
+    setOpen(true)
+  }
+
+
   return (
     <div className="main-container">
-      <Navbar />
-      <div className="sidebar-and-editor-container">
-        <Leftsidebar />
-        <Pageeditor />
-        <Rightsidebar />
+      <div className="sidebar">
+        <LeftSidebar />
+      </div>
+      <div className="insert-link-section">
+        <div className="input-container">
+          <input type="text" className="custom-input" placeholder="Upload file here" value={inputValue}  readOnly onClick={() => document.getElementById("fileInput")?.click()}  />
+          <input type="file" id="fileInput" style={{ display: "none" }} onChange={handleFileUpload} />
+          <Button onClick={handleOpen} className='upload-btn'>
+            <InsertLink />
+          </Button>
+        </div>
+        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" >
+          <div>
+            <h3>PDF upload</h3>
+          </div>
+        </Modal>
       </div>
     </div>
   )
